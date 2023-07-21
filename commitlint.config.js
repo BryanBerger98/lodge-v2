c773c61fca77f7d497da41a7bbad52827ec13be4
Bryan BERGER <contact@weberger.net>
2023-07-21 21:55:16 +0200

    fix: commitlint config (#103)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,4 @@
-module.exports = { extends: [ '@commitlint/config-conventional' ] };
+module.exports = {
+	extends: [ '@commitlint/config-conventional' ],
+	rules: { 'footer-max-line-length': [ 0, 'always' ] },
+};
