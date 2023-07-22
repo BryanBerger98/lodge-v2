1a2055dcbbeccfe9ea50d9fd9a2d4453ce77cdc7
Bryan BERGER <contact@weberger.net>
2023-07-22 12:42:34 +0200

    chore: CI/CD improvements (#118)

--- build.sh
+++ build.sh
@@ -3,17 +3,11 @@
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
 if [[ "$VERCEL_GIT_COMMIT_REF" == "main" || "$VERCEL_GIT_COMMIT_REF" == "develop" ]] ; then
-  if npx turbo-ignore 2> /dev/null; then
-    # Don't build
-    echo "🛑 - Build cancelled"
-    exit 0;
-  else
-    # Proceed with the build
-    echo "✅ - Build can proceed"
-    exit 1;
-  fi
+	# Proceed with the build
+	echo "✅ - Build can proceed"
+	exit 1;
 else
-  # Don't build
-  echo "🛑 - Build cancelled"
-  exit 0;
+	# Don't build
+	echo "🛑 - Build cancelled"
+	exit 0;
 fi
